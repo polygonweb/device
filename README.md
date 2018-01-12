0b079ee6b7db7d4bb22da1be28180efcc9f0efd7
monochromer <monochromer@mail.ru>
2018-01-12 13:08:13 +0500

    deploy

--- README.md
+++
@@ -1,8 +0,0 @@
-# Device
-
-[Demo](https://polygonweb.github.io/device)
-
-[Project code](https://github.com/polygonweb/device)
-
-[Template code](https://github.com/monochromer/polygon-template)
-
--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-process.on('uncaughtException', function(err) {
-    console.log('\n================\n');
-    console.error(err.message, err.stack, err.errors);
-    console.log('\n================\n');
-    process.exit(255);
-});
-
-// require('babel-register');
-require('./gulp');
